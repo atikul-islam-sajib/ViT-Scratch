732057dd307d3a0e61f640077e3727563b68abdb
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-08-02 17:24:05 +0200

    scaled dot product passed the unittest

--- unittest/test.py
+++ unittest/test.py
@@ -7,20 +7,47 @@ from torch.utils.data import DataLoader, TensorDataset
 sys.path.append("./src/")
 
 from positional_encoding import PositionalEncoding
+from scaled_dot_product import scaled_dot_product_attention
 
 
 class UnitTest(unittest.TestCase):
     def setUp(self):
         self.total_texts = 400
         self.batch_size = 40
+        self.nheads = 8
         self.sequence_length = 200
         self.dimension = 512
         self.constant = 10000
+
+        self.query = torch.randn(self.batch_size, self.sequence_length, self.dimension)
+        self.key = torch.randn(self.batch_size, self.sequence_length, self.dimension)
+        self.value = torch.randn(self.batch_size, self.sequence_length, self.dimension)
 
         self.positional_encoding = PositionalEncoding(
             sequence_length=self.sequence_length,
             dimension=self.dimension,
             constant=self.constant,
+        )
+
+        self.attention = scaled_dot_product_attention(
+            query=self.query.view(
+                self.query.size(0),
+                self.nheads,
+                self.sequence_length,
+                self.dimension // self.nheads,
+            ),
+            key=self.key.view(
+                self.key.size(0),
+                self.nheads,
+                self.sequence_length,
+                self.dimension // self.nheads,
+            ),
+            value=self.value.view(
+                self.value.size(0),
+                self.nheads,
+                self.sequence_length,
+                self.dimension // self.nheads,
+            ),
         )
 
     def test_positional_encoding(self):
@@ -78,6 +105,17 @@ class UnitTest(unittest.TestCase):
         self.assertEqual(
             embeddings_with_positional.size(),
             (self.batch_size, self.sequence_length, self.dimension),
+        )
+
+    def test_scaled_dot_product(self):
+        self.assertEqual(
+            self.attention.size(),
+            (
+                self.batch_size,
+                self.nheads,
+                self.sequence_length,
+                self.dimension // self.nheads,
+            ),
         )
 
 
