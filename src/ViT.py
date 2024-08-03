fe9fedbaac0424eda8594e31441e5dab11dfdb3c
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-08-03 15:40:02 +0200

    ViT is completed

--- src/ViT.py
+++ src/ViT.py
@@ -16,7 +16,7 @@ class ViT(nn.Module):
         image_channels: int = 3,
         image_size: int = 224,
         labels: int = 4,
-        path_size: int = 16,
+        patch_size: int = 16,
         nheads: int = 8,
         num_encoder_layers: int = 8,
         dropout: float = 0.1,
@@ -31,7 +31,7 @@ class ViT(nn.Module):
         self.image_channels = image_channels
         self.image_size = image_size
         self.labels = labels
-        self.path_size = path_size
+        self.patch_size = patch_size
         self.nheads = nheads
         self.num_encoder_layers = num_encoder_layers
         self.dropout = dropout
@@ -40,8 +40,8 @@ class ViT(nn.Module):
         self.activation = activation
         self.bias = bias
 
-        self.num_patches = (self.image_size // self.path_size) ** 2
-        self.num_dimension = (self.path_size**2) * self.image_channels
+        self.num_patches = (self.image_size // self.patch_size) ** 2
+        self.num_dimension = (self.patch_size**2) * self.image_channels
 
         assert (
             self.num_dimension % self.nheads == 0
@@ -54,7 +54,7 @@ class ViT(nn.Module):
         self.patch_embedding = PatchEmbedding(
             image_size=self.image_size,
             image_channels=self.image_channels,
-            patch_size=self.path_size,
+            patch_size=self.patch_size,
         )
 
         self.transformerEncoder = TransformerEncoder(
@@ -91,17 +91,97 @@ class ViT(nn.Module):
 
 
 if __name__ == "__main__":
-    vision_transformer = ViT(
-        image_channels=3,
-        image_size=224,
-        path_size=16,
-        nheads=8,
-        num_encoder_layers=2,
-        dropout=0.1,
-        dim_feedforward=2048,
-        epsilon=1e-5,
-        activation="relu",
-        bias=True,
+    parser = argparse.ArgumentParser(description="Vit Model".title())
+    parser.add_argument(
+        "--image_channels",
+        type=int,
+        default=config()["dataloader"]["channels"],
+        help="Number of image channels".capitalize(),
+    )
+    parser.add_argument(
+        "--image_size",
+        type=int,
+        default=config()["dataloader"]["image_size"],
+        help="Image size".capitalize(),
+    )
+    parser.add_argument(
+        "--labels",
+        type=int,
+        default=len(config()["dataloader"]["labels"]),
+        help="Number of labels".capitalize(),
+    )
+    parser.add_argument(
+        "--patch_size",
+        type=int,
+        default=config()["ViT"]["patch_size"],
+        help="patch_size size".capitalize(),
+    )
+    parser.add_argument(
+        "--nheads",
+        type=int,
+        default=config()["ViT"]["nheads"],
+        help="Number of heads".capitalize(),
+    )
+    parser.add_argument(
+        "--num_encoder_layers",
+        type=int,
+        default=config()["ViT"]["num_layers"],
+        help="Number of encoder layers".capitalize(),
+    )
+    parser.add_argument(
+        "--dropout",
+        type=float,
+        default=config()["ViT"]["dropout"],
+        help="Dropout rate".capitalize(),
+    )
+    parser.add_argument(
+        "--dim_feedforward",
+        type=int,
+        default=config()["ViT"]["dim_feedforward"],
+        help="Dimension of feedforward".capitalize(),
+    )
+    parser.add_argument(
+        "--epsilon",
+        type=float,
+        default=config()["ViT"]["eps"],
+        help="Epsilon".capitalize(),
+    )
+    parser.add_argument(
+        "--activation",
+        type=str,
+        default=config()["ViT"]["activation"],
+        help="Activation function".capitalize(),
+    )
+    parser.add_argument(
+        "--bias",
+        type=bool,
+        default=True,
+        help="Bias".capitalize(),
     )
 
-    print(vision_transformer(torch.randn(64, 3, 224, 224)).size())
+    args = parser.parse_args()
+
+    vision_transformer = ViT(
+        image_channels=args.image_channels,
+        image_size=args.image_size,
+        labels=args.labels,
+        patch_size=args.patch_size,
+        nheads=args.nheads,
+        num_encoder_layers=args.num_encoder_layers,
+        dropout=args.dropout,
+        dim_feedforward=args.dim_feedforward,
+        epsilon=args.epsilon,
+        activation=args.activation,
+        bias=args.bias,
+    )
+
+    batch_size = config()["dataloader"]["batch_size"]
+
+    assert vision_transformer(
+        torch.randn(
+            batch_size,
+            args.image_channels,
+            args.image_size,
+            args.image_size,
+        )
+    ).size() == (batch_size, args.labels)
