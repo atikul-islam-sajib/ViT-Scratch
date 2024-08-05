e124f8b454810271133a0005a966a689e63eb5ea
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-08-06 01:45:19 +0200

    helper method associated with trainer

--- src/trainer.py
+++ src/trainer.py
@@ -3,6 +3,12 @@ import torch
 import argparse
 import torch.nn as nn
 
+sys.path.append(".src/")
+
+from ViT import ViT
+from helper import helpers
+from loss import CategoricalLoss
+
 
 class Trainer:
     def __init__(
@@ -22,6 +28,7 @@ class Trainer:
         lr: float = 0.0001,
         beta1: float = 0.5,
         beta2: float = 0.999,
+        momentum: float = 0.9,
         step_size: int = 10,
         gamma: float = 0.85,
         threshold: int = 100,
@@ -49,6 +56,7 @@ class Trainer:
         self.lr = lr
         self.beta1 = beta1
         self.beta2 = beta2
+        self.momentum = momentum
         self.step_size = step_size
         self.gamma = gamma
         self.threshold = threshold
@@ -60,13 +68,54 @@ class Trainer:
         self.elasticnet_regularization = elasticnet_regularization
         self.verbose = verbose
 
-    def l1_loss(self, model):
+        self.init = helpers(
+            image_channels=self.image_channels,
+            image_size=self.image_size,
+            labels=self.labels,
+            patch_size=self.patch_size,
+            nheads=self.nheads,
+            num_encoder_layers=self.num_encoder_layers,
+            dropout=self.dropout,
+            dim_feedforward=self.dim_feedforward,
+            epsilon=self.epsilon,
+            activation=self.activation,
+            bias=self.bias,
+            lr=self.lr,
+            beta1=self.beta1,
+            beta2=self.beta2,
+            momentum=self.momentum,
+            adam=self.adam,
+            SGD=self.SGD,
+        )
+
+        self.train_dataloader = self.init["train_dataloader"]
+        self.valid_dataloader = self.init["valid_dataloader"]
+        self.model = self.init["model"]
+        self.optimizer = self.init["optimizer"]
+        self.criterion = self.init["criterion"]
+
+        assert (
+            self.init["train_dataloader"].__class__ == torch.utils.data.DataLoader
+        ), "Dataloader is not a torch.utils.data.DataLoader".capitalize()
+        assert (
+            self.init["valid_dataloader"].__class__ == torch.utils.data.DataLoader
+        ), "Dataloader is not a torch.utils.data.DataLoader".capitalize()
+
+        assert self.init["model"].__class__ == ViT, "Model is not a ViT".capitalize()
+        assert (
+            self.init["optimizer"].__class__ == torch.optim.Adam
+        ), "Optimizer is not a Adam".capitalize()
+        assert (
+            self.init["criterion"].__class__ == CategoricalLoss
+        ), "Loss is not a CategoricalLoss".capitalize()
+
+    def l1_loss(self, model: ViT):
         pass
 
-    def l2_loss(self, model):
+    def l2_loss(self, model: ViT):
         pass
 
-    def elasticnet_loss(self, model):
+    def elasticnet_loss(self, model: ViT):
         pass
 
     def saved_checkpoints(self, **kwargs):
@@ -81,3 +130,7 @@ class Trainer:
     @staticmethod
     def display_history():
         pass
+
+
+if __name__ == "__main__":
+    trainer = Trainer()
