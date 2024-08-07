38424fe98c748bda1bf28b2b3f6d7c0c138ad539
Atikul Islam Sajib <112280249+atikul-islam-sajib@users.noreply.github.com>
2024-08-08 00:35:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,28 @@
 from setuptools import setup, find_packages
 
-
-def requirements():
-    with open("./requirements.txt", "r") as file:
-        return file.read().splitlines()
-
-
 setup(
     name="ViT",
     version="0.0.1",
-    description="A deep learning project that is build for Transformer based task using ViT",
+    description="A deep learning project for Transformer-based tasks using ViT",
     author="Atikul Islam Sajib",
     author_email="atikulislamsajib137@gmail.com",
-    url="https://github.com/atikul-islam-sajib/ViT-Scratch.git",  # Update with your project's GitHub repository URL
+    url="https://github.com/atikul-islam-sajib/ViT-Scratch",
     packages=find_packages(),
-    install_requires=requirements(),
+    install_requires=[
+        "numpy==1.21.2",
+        "pandas==1.3.3",
+        "torch==1.9.0",
+    ],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.9",
     ],
-    keywords="TransformerScratch : machine-learning",
+    keywords="TransformerScratch, machine-learning",
     project_urls={
-        "Bug Tracker": "https://github.com/atikul-islam-sajib/ViT-Scratch.git/issues",
-        "Documentation": "https://github.com/atikul-islam-sajib/ViT-Scratch.git",
-        "Source Code": "https://github.com/atikul-islam-sajib/ViT-Scratch.git",
+        "Bug Tracker": "https://github.com/atikul-islam-sajib/ViT-Scratch/issues",
+        "Documentation": "https://github.com/atikul-islam-sajib/ViT-Scratch",
+        "Source Code": "https://github.com/atikul-islam-sajib/ViT-Scratch",
     },
 )
